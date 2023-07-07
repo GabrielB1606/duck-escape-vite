530288135c8bae2c850e74910ca40e3e23b658b0
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-07-07 18:00:12 -0400

    some doc and polishing on the boiler plate

--- main.js
+++ main.js
@@ -1,127 +1,122 @@
-import {glMatrix, mat4} from 'gl-matrix'
+import { glMatrix, mat4 } from 'gl-matrix'
 
-const vertexShaderText = `
-    attribute vec3 a_position;
-  
-    uniform mat4 u_model;
-    uniform mat4 u_view;
-    uniform mat4 u_proj;
+import vertexShaderText from './shaders/vertex.glsl'
+import fragmentShaderText from './shaders/fragment.glsl'
 
-    void main() {
-        gl_Position = u_proj * u_view * u_model * vec4(a_position, 1.0);
-    }
-`;
+const DELTA_TIME = 1000 / 24;
+const canvas = document.getElementById("game");
 
-const fragmentShaderText = `
-    precision mediump float;
-  
-    void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    }
-`;
+const PX_WIDTH = 1 / 640;
+const PX_HEIGHT = 1 / 480;
 
-const InitDemo = () =>{
-    console.log("loading");
+const vertices = [
+  -1.0, 1.0, -1.0,
+  1.0, 1.0, -1.0,
+  -1.0, -1.0, -1.0,
+  1.0, -1.0, -1.0,
+];
 
-    const canvas = document.getElementById("game");
-    const gl = canvas.getContext('webgl');
+const InitDemo = () => {
 
-    if(!gl){
-        gl = canvas.getContext('experimental-webgl');
-    }
+  // get openGL context
+  var gl = canvas.getContext('webgl');
 
-    if(!gl){
-        console.log("browser not compatible");
-    }
+  if (!gl) {
+    gl = canvas.getContext('experimental-webgl');
+  }
 
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  if (!gl) {
+    console.log("browser not compatible");
+  }
 
-    gl.shaderSource(vertexShader, vertexShaderText);
-    gl.shaderSource(fragmentShader, fragmentShaderText);
+  // initialize shaders
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
-    gl.compileShader(vertexShader);
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-		console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
-		return;
-	}
+  // get shader source
+  gl.shaderSource(vertexShader, vertexShaderText);
+  gl.shaderSource(fragmentShader, fragmentShaderText);
 
-    gl.compileShader(fragmentShader);
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-		console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
-		return;
-	}
+  // compile shaders
+  gl.compileShader(vertexShader);
+  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+    return;
+  }
 
-    const shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-		console.error('ERROR linking program!', gl.getProgramInfoLog(shaderProgram));
-		return;
-	}
+  gl.compileShader(fragmentShader);
+  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+    return;
+  }
 
-    gl.validateProgram(shaderProgram);
-	if (!gl.getProgramParameter(shaderProgram, gl.VALIDATE_STATUS)) {
-		console.error('ERROR validating program!', gl.getProgramInfoLog(shaderProgram));
-		return;
-	}
+  // create shader program
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  // validate shader program
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    console.error('ERROR linking program!', gl.getProgramInfoLog(shaderProgram));
+    return;
+  }
+
+  gl.validateProgram(shaderProgram);
+  if (!gl.getProgramParameter(shaderProgram, gl.VALIDATE_STATUS)) {
+    console.error('ERROR validating program!', gl.getProgramInfoLog(shaderProgram));
+    return;
+  }
+
+  // initialize attributes
+  gl.useProgram(shaderProgram);
+
+  // position buffer
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+  const positionAttributeLocation = gl.getAttribLocation(shaderProgram, "a_position");
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+
+  // initialize uniforms
+  const u_model_loc = gl.getUniformLocation(shaderProgram, "u_model");
+  const u_view_loc = gl.getUniformLocation(shaderProgram, "u_view");
+  const u_proj_loc = gl.getUniformLocation(shaderProgram, "u_proj");
+
+  // matrices
+  let modelMatrix = new Float32Array(16);
+  let viewMatrix = new Float32Array(16);
+  let projMatrix = new Float32Array(16);
+
+  mat4.identity(modelMatrix);
+  mat4.scale(modelMatrix, modelMatrix, [0.2, 0.2, 1.0]);
+  mat4.identity(viewMatrix);
+  mat4.identity(projMatrix);
+  mat4.perspective(projMatrix, glMatrix.toRadian(45), 4 / 3, 0.1, 100.0);
+
+  // send uniforms
+  gl.uniformMatrix4fv(u_model_loc, gl.FALSE, modelMatrix);
+  gl.uniformMatrix4fv(u_view_loc, gl.FALSE, viewMatrix);
+  gl.uniformMatrix4fv(u_proj_loc, gl.FALSE, projMatrix);
+
+  const gameLoop = () => {
 
     gl.useProgram(shaderProgram);
 
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // mat4.translate(modelMatrix, modelMatrix, [ 0.001* performance.now()/1000, 0.0, 0.0]);
+    // gl.uniformMatrix4fv(u_model_loc, gl.FALSE, modelMatrix);
 
-    const vertices = [
-       -1.0,  1.0, -1.0,
-        1.0,  1.0, -1.0,
-       -1.0, -1.0, -1.0,
-        1.0, -1.0, -1.0,
-    ];
+    gl.clearColor(0.2, 0.6, 0.2, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length / 3);
 
-    const positionAttributeLocation = gl.getAttribLocation(shaderProgram, "a_position");
-    gl.enableVertexAttribArray(positionAttributeLocation);
-    gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+  }
 
-
-    gl.useProgram(shaderProgram);
-
-    const u_model_loc = gl.getUniformLocation(shaderProgram, "u_model");
-    const u_view_loc = gl.getUniformLocation(shaderProgram, "u_view");
-    const u_proj_loc = gl.getUniformLocation(shaderProgram, "u_proj");
-
-    let modelMatrix = new Float32Array(16);
-    let viewMatrix = new Float32Array(16);
-    let projMatrix = new Float32Array(16);
-
-    mat4.identity(modelMatrix);
-    mat4.scale(modelMatrix, modelMatrix, [0.2, 0.2, 1.0]);
-    mat4.identity(viewMatrix);
-    mat4.identity(projMatrix);
-    mat4.perspective(projMatrix, glMatrix.toRadian(45), 4/3, 0.1, 100.0);
-
-    gl.uniformMatrix4fv(u_model_loc, gl.FALSE, modelMatrix);
-    gl.uniformMatrix4fv(u_view_loc,  gl.FALSE, viewMatrix);
-    gl.uniformMatrix4fv(u_proj_loc,  gl.FALSE, projMatrix);
-
-    const gameLoop = () => {
-
-      gl.useProgram(shaderProgram);
-      
-      // mat4.translate(modelMatrix, modelMatrix, [ 0.001* performance.now()/1000, 0.0, 0.0]);
-      // gl.uniformMatrix4fv(u_model_loc, gl.FALSE, modelMatrix);
-      
-      gl.clearColor(0.2, 0.6, 0.2, 1.0);
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      
-      gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length/3);
-
-    }
-
-    setInterval(gameLoop, 1000/24);
-
+  setInterval(gameLoop, DELTA_TIME);
 
 }
 
